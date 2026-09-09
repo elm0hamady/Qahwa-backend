ae47cfa2f99e92952c8f5e517bc0ba7b5663ebdb
elm0hamady <melmohamady95@gmail.com>
2026-09-09 10:36:33 +0300

    allow any for topics end point

--- backend/apps/bank/views.py
+++ backend/apps/bank/views.py
@@ -3,14 +3,14 @@ from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework import generics 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from .models import Topic
 from .services import get_ready_topics
 from .serializers import TopicSerializer
 
 class TopicListView(generics.ListAPIView):
     serializer_class = TopicSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['category']
     search_fields = ['name']
